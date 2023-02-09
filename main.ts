482cf04f8aa56c5ca1d747b04b3e8eb6371b9681
petteren <104796792+petteren@users.noreply.github.com>
2023-02-09 13:40:13 +0100

    Oppdater main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     while (true) {
         basic.showIcon(IconNames.Giraffe)
         if (RingbitCar.ringbitcar_sonarbit(RingbitCar.Distance_Unit.Distance_Unit_cm) < 10) {
             basic.showIcon(IconNames.Ghost)
         }
-        
     }
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     while (true) {
         RingbitCar.running_time(RingbitCar.Direction_run.forward, 10)
         if (RingbitCar.ringbitcar_sonarbit(RingbitCar.Distance_Unit.Distance_Unit_cm) < 10) {
@@ -16,7 +15,6 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
             basic.pause(1000)
             RingbitCar.running_time(RingbitCar.Direction_run.forward, 10)
         }
-        
         basic.showIcon(IconNames.Heart)
     }
 })
